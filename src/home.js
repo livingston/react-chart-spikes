06c5d0db17c5091e92854aee4767f193fc57827a
Livingston Samuel <livingston.samuel@gmail.com>
2017-09-01 17:09:23 +0530

    unlinking unused components

--- src/home.js
+++ src/home.js
@@ -6,10 +6,6 @@ import {
   Switch
 } from 'react-router-dom';
 
-import VictoryCharts from './victoryCharts';
-import RumbleCharts from './rumbleCharts';
-import ReCharts from './rechartCharts';
-import D3Charts from './d3Charts';
 import Final from './final';
 
 const NotFound = ({ location }) => (
@@ -30,20 +26,12 @@ class Home extends Component {
         <nav>
           <ul>
             <li><NavLink exact={true} to="/">Dashboard</NavLink></li>
-            <li><NavLink to="/recharts">Recharts</NavLink></li>
-            <li><NavLink to="/victory">Victory</NavLink></li>
-            {/* <li><NavLink to="/rumble">Rumble</NavLink></li> */}
-            {/* <li><NavLink to="/d3">D3</NavLink></li> */}
           </ul>
         </nav>
 
         <article>
           <Switch>
             <Route exact path="/" component={Final}/>
-            <Route path="/rumble" component={RumbleCharts}/>
-            <Route path="/recharts" component={ReCharts}/>
-            <Route path="/d3" component={D3Charts}/>
-            <Route path="/victory" component={VictoryCharts}/>
             <Route component={NotFound}/>
           </Switch>
         </article>

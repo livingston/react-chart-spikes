975fad5088d3b66b45c822007aa5130d118f3264
Livingston Samuel <livingston.samuel@gmail.com>
2017-09-26 01:45:49 +0530

    removing fixed-data-table & react-table spikes

--- src/app.js
+++ src/app.js
@@ -7,13 +7,10 @@ import {
   Link,
   Switch
 } from 'react-router-dom';
-import { LicenseManager } from "ag-grid-enterprise/main";
 
 import './styles/app.scss';
 
 import Home from 'home';
-
-LicenseManager.setLicenseKey("ag-Grid_Evaluation_License_Not_for_Production_1Devs15_November_2017__MTUxMDcwNDAwMDAwMA==3c862d06679ff2da4f8d4ac677bff980");
 
 const App = () => (<Router>
   <Home />
